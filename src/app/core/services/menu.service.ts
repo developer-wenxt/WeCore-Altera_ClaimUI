22480c0cb2ffe4beff55a1a053c4c04aa080e440
intern002 <trainee@wenxttech.com>
2026-09-16 13:00:53 +0530

    fixing the sso issue

--- src/app/core/services/menu.service.ts
+++ src/app/core/services/menu.service.ts
@@ -9,7 +9,10 @@ import { FieldConfig } from '../models/model';
 
 @Injectable({ providedIn: 'root' })
 export class MenuService {
-  private baseUrl = environment.apiUrl;
+   
+  private get baseUrl(): string {
+    return environment.apiUrl;
+  }
 
   constructor(private http: HttpClient) { }
 
